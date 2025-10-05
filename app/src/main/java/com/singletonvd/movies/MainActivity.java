047bbef26d30204c615f5c0fc643e16976349af0
SingletonVD <singletonvd@mail.ru>
2025-10-05 04:01:06 +0300

    feat: move logic to ViewModel

--- app/src/main/java/com/singletonvd/movies/MainActivity.java
+++ app/src/main/java/com/singletonvd/movies/MainActivity.java
@@ -4,25 +4,26 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.schedulers.Schedulers;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ApiService apiService = ApiFactory.apiService;
     private static final String LOG_TAG = "MainActivity";
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        apiService.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(moviesResponse -> {
-                    Log.d(LOG_TAG, String.valueOf(moviesResponse.getMovies().size()));
-                });
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getMovies().observe(
+                this,
+                movies -> Log.d(LOG_TAG, movies.toString())
+        );
+
+        if (!viewModel.getMovies().isInitialized()) {
+            viewModel.loadMovies();
+        }
     }
 }
---
+++ app/src/main/java/com/singletonvd/movies/MainViewModel.java
@@ -0,0 +1,76 @@
+package com.singletonvd.movies;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private static final String LOG_TAG = "MainViewModel";
+
+    private final MutableLiveData<List<Movie>> movies = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isError = new MutableLiveData<>(false);
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private final ApiService apiService = ApiFactory.apiService;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void loadMovies() {
+        int firstPage = 1;
+        loadMovies(firstPage);
+    }
+
+    public void loadMovies(int page) {
+        Disposable disposable = apiService.loadMovies(page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(d -> {
+                    isLoading.setValue(true);
+                    isError.setValue(false);
+                })
+                .doAfterTerminate(() -> isLoading.setValue(false))
+                .subscribe(
+                        moviesResponse -> movies.setValue(moviesResponse.getMovies()),
+                        error -> {
+                            isError.setValue(true);
+                            Log.d(
+                                    LOG_TAG,
+                                    String.format("Error loading movies: %s", error.toString())
+                            );
+                        }
+                );
+        compositeDisposable.add(disposable);
+    }
+
+    public LiveData<List<Movie>> getMovies() {
+        return movies;
+    }
+
+    public MutableLiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+
+    public MutableLiveData<Boolean> getIsError() {
+        return isError;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
+    }
+}
